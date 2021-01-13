6c5101a92231eb4e858cf108814232b4c9205669
Martin Yeo <martin.yeo@metoffice.gov.uk>
2021-01-13 19:57:13 +0000

    Static credit comment.

--- iris/logo/generate_logo.py
+++ iris/logo/generate_logo.py
@@ -427,13 +427,7 @@ def _write_svg_file(filename, svg_root, write_dir=None, zip_archive=None):
     """Format the svg then write the svg to a file in write_dir, or
     optionally to an open ZipFile."""
     # Add a credit comment at top of SVG.
-    repo_name = "marketing"
-    script_path = Path(__file__)
-    repo_dir = [
-        parent for parent in script_path.parents if parent.name == repo_name
-    ][0]
-    script_path_relative = script_path.relative_to(repo_dir.parent)
-    comment = f"Created by https://github.com/SciTools/{script_path_relative}"
+    comment = f"Created by https://github.com/SciTools/marketing/iris/logo/generate_logo.py"
     svg_root.insert(0, ET.Comment(comment))
 
     input_string = ET.tostring(svg_root)
